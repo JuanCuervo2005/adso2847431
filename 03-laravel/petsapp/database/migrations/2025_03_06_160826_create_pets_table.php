42778fa841ee8c1f6d182100f2b1cbeeb3dab29c
JuanCuervo2005 <juanescg2001@gmail.com>
2025-03-06 11:34:18 -0500

    se modifico

--- 03-laravel/petsapp/database/migrations/2025_03_06_160826_create_pets_table.php
+++ 03-laravel/petsapp/database/migrations/2025_03_06_160826_create_pets_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('pets', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('photo')->default('no-image.png');
+            $table->string('kind');
+            $table->integer('weight');
+            $table->integer('age');
+            $table->string('breed');
+            $table->string('location');
             $table->timestamps();
         });
     }
